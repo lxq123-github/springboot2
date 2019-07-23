72bc6d181650308399ed297c730fb4dcb6e05a2c
lxq <lixiangqi-github>
2019-07-24 05:47:06 +0800

    springboot2  20190722 三次review进行修改

--- src/main/java/com/lxq/springboot/controller/OrderMgrController.java
+++ src/main/java/com/lxq/springboot/controller/OrderMgrController.java
@@ -7,17 +7,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 订单管理控制器
  */
-@Controller
+@RestController
 @RequestMapping(value="/order")
 @Api(value = "订单管理控制器")
 public class OrderMgrController {
@@ -27,69 +22,38 @@ public class OrderMgrController {
 
     /**
      * 显示订单信息
-     * @param pages
+     * @param id
      * @return
      */
-    @ApiOperation(value="显示订单信息", notes="根据页码显示订单信息")
-    @ApiImplicitParam(name = "pages", value = "当前页码", required = true, dataType = "Integer", paramType = "path")
-    @GetMapping
-    public ModelAndView show(Integer pages) throws Exception{
-        pages = pages==null?1:pages;
-        PageInfo pageInfo= orderService.findAll(pages,5);
-        ModelAndView mv = new ModelAndView("order/show");
-        mv.addObject("orders",pageInfo);
-        return mv;
+    @ApiOperation(value="显示订单", notes="根据页码显示订单信息")
+    @ApiImplicitParam(name = "id", value = "当前页码", required = true, dataType = "Integer", paramType = "path")
+    @GetMapping(value="/list")
+    public PageInfo show(@RequestParam(name="id", defaultValue="1") Integer id) throws Exception{
+        return orderService.findAll(id,5);
     }
 
     /**
-     * 跳转页
-     * @param pages
+     * 显示一个订单信息
+     * @param id
      * @return
      */
-    @ApiOperation(value="跳转订单信息", notes="根据页码跳转订单信息")
-    @ApiImplicitParam(name = "pages", value = "当前页码", required = true, dataType = "Integer", paramType = "path")
-    @GetMapping(value = "/go/{pages}")
-    public ModelAndView go(@PathVariable Integer pages) {
-        pages = pages<1?1:pages;
-        return new ModelAndView(REDIRECT_ORDER + "?pages="+pages);
+    @ApiOperation(value="显示一个订单", notes="根据订单ID显示一个信息")
+    @ApiImplicitParam(name = "id", value = "订单", required = true, dataType = "Integer", paramType = "path")
+    @GetMapping(value="/{id}")
+    public OrderForm getOrder(@PathVariable Integer id) throws Exception{
+        return orderService.findById(id);
     }
 
     /**
      * 删除一个订单
-     * @param oid
+     * @param id
      * @return
      */
-    @ApiOperation(value="删除订单ID信息", notes="根据页码删除订单信息")
-    @ApiImplicitParam(name = "pages", value = "当前页码", required = true, dataType = "Integer", paramType = "path")
-    @GetMapping(value="/delete/{oid}")
-    public ModelAndView delete(@PathVariable Integer oid) throws Exception{
-        orderService.deleteOrder(oid);
-        return new ModelAndView(REDIRECT_ORDER);
-    }
-
-    /**
-     * 跳转更新订单
-     * @param oid
-     * @return
-     */
-    @ApiOperation(value="更新订单ID信息", notes="根据页码更新订单信息")
-    @ApiImplicitParam(name = "oid", value = "当前订单id", required = true, dataType = "Integer", paramType = "path")
-    @GetMapping(value="/update/{oid}")
-    public ModelAndView update(@PathVariable Integer oid) throws Exception{
-        OrderForm order = orderService.findById(oid);
-        ModelAndView mv = new ModelAndView("order/update");
-        mv.addObject("order",order);
-        return mv;
-    }
-
-    /**
-     * 跳转添加订单
-     * @return
-     */
-    @ApiOperation(value="跳转add页面", notes="跳转add页面")
-    @GetMapping(value="/add")
-    public String add() throws Exception{
-        return "order/add";
+    @ApiOperation(value="删除订单", notes="根据订单ID删除信息")
+    @ApiImplicitParam(name = "id", value = "订单ID", required = true, dataType = "Integer", paramType = "path")
+    @DeleteMapping(value="/{id}")
+    public int delete(@PathVariable Integer id) throws Exception{
+        return orderService.deleteOrder(id);
     }
 
     /**
@@ -97,27 +61,37 @@ public class OrderMgrController {
      * @param order
      * @return
      */
-    @ApiOperation(value="更新订单信息", notes="更新订单信息")
-    @ApiImplicitParam(name = "order", value = "订单对象", required = true, dataType = "OrderPojo", paramType = "path")
-    @PostMapping(value="/updateDate")
-    public String save(OrderForm order) throws Exception{
-        if(order.getOrderId()!=null)
-            orderService.updateOrder(order);
-        else
-            orderService.saveOrder(order);
-        return REDIRECT_ORDER;
+    @ApiOperation(value="增加订单", notes="增加订单信息")
+    @ApiImplicitParam(name = "order", value = "订单对象", required = true, dataType = "OrderForm", paramType = "path")
+    @PostMapping
+    public int save(OrderForm order) throws Exception{
+        return orderService.saveOrder(order);
     }
 
     /**
+     * 更新订单
+     * @param order
+     * @return
+     */
+    @ApiOperation(value="更新订单", notes="更新订单信息")
+    @ApiImplicitParam(name = "order", value = "订单对象", required = true, dataType = "OrderForm", paramType = "path")
+    @PutMapping
+    public int update(OrderForm order) throws Exception{
+        return orderService.updateOrder(order);
+    }
+
+/*    GET /ec2/instance/batch?id=aa,bb,cc：简约的设计
+    GET /ec2/instance?batch={"ids":["aa","bb","cc"]}：健壮的设计
+    GET /ec2/instance?batch=[{"method":"DELETE","id":"aa"},{"method":"DELETE","id":"bb"},{"method":"DELETE","id":"cc"}]：facebook 的设计（墙外）
+    *//**
      * 批量删除订单
      * @param ids
      * @return
-     */
-    @ApiOperation(value="删除订单", notes="删除订单")
+     *//*
+    @ApiOperation(value="批量删除订单", notes="批量删除订单")
     @ApiImplicitParam(name = "ids", value = "String[]", required = true, dataType = "String[]", paramType = "path")
-    @PostMapping(value="/batchDelete")
-    public String batchDelete(String[] ids) throws Exception{
+    @DeleteMapping
+    public void batchDelete(String[] ids) throws Exception{
         orderService.deleteBatch(ids);
-        return REDIRECT_ORDER;
-    }
+    }*/
 }
